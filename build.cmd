0b63e1c07ce9786a790278e3fa69094e61a61fcd
Carlos Tronco <cars@lostroncos.org>
2019-09-06 09:34:02 -0700

    changes requested by @markpeek

--- build.cmd
+++
@@ -1,9 +0,0 @@
-cd S:\gosrc\src\github.com\cars\terraform-provider-vra7-1
-go build
-copy terraform-provider-vra7.exe /Y ..\test\example\simple\.terraform\plugins\windows_amd64
-pushd S:\gosrc\src\github.com\cars\test\example\simple
-del log
-del vra-terraform.log
-terraform init
-terraform refresh
-popd
