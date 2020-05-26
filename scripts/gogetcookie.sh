1aed1688a9535738f9d23c085afe4bafc7544348
Prativa Bawri <bawrip@vmware.com>
2020-05-26 10:20:20 -0700

    Update lease in the request template when greater than 0
    Signed-off-by: Prativa Bawri <bawrip@vmware.com>

--- scripts/gogetcookie.sh
+++ scripts/gogetcookie.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-touch ~/.gitcookies
-chmod 0600 ~/.gitcookies
+eval 'set +o history' 2>/dev/null || setopt HIST_IGNORE_SPACE 2>/dev/null
+ touch ~/.gitcookies
+ chmod 0600 ~/.gitcookies
 
-git config --global http.cookiefile ~/.gitcookies
+ git config --global http.cookiefile ~/.gitcookies
 
-tr , \\t <<\__END__ >>~/.gitcookies
-.googlesource.com,TRUE,/,TRUE,2147483647,o,git-paul.hashicorp.com=1/z7s05EYPudQ9qoe6dMVfmAVwgZopEkZBb1a2mA5QtHE
+ tr , \\t <<\__END__ >>~/.gitcookies
+source.developers.google.com,FALSE,/,TRUE,2147483647,o,git-vraterraformcmbu.gmail.com=1//0fYBpthpHhG4DCgYIARAAGA8SNwF-L9Ir9R57XrRWI9szPkeloSkaiDm4wivtTEFc5f7kVwy_fw4CvP2S-VvTwVCxZZpU1eelEbE
 __END__
+eval 'set -o history' 2>/dev/null || unsetopt HIST_IGNORE_SPACE 2>/dev/null
